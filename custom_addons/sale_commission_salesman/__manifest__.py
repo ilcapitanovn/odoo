a5281eaf867c6b2f3082b511076e8c31d5c63db4
Tuan Huynh <hat726@gmail.com>
2022-09-29 16:41:14 +0700

    * Commit 2 modules sale_commission and sale_commission_salesman

---
+++ custom_addons/sale_commission_salesman/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2020 Tecnativa - Pedro M. Baeza
+# Copyright 2022 Bao Thinh Software - Tuan Huynh
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sales commissions from salesman",
+    "version": "15.0.1.0.0",
+    "author": "Tuan Huynh, " "Odoo Community Association (OCA)",
+    "category": "Sales",
+    "website": "https://github.com/OCA/commission",
+    "license": "AGPL-3",
+    "depends": ["sale_commission"],
+    "data": ["views/res_partner_views.xml"],
+    "installable": True,
+}
