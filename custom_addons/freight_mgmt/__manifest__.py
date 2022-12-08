8fac991fc553a68ce1dfb082402b99b85fb21fa9
Tuan Huynh <hat726@gmail.com>
2022-12-08 07:53:35 +0700

    * Freight version 15.0.2.0.2 - Fix issue create duplicate Supplier with contact

--- custom_addons/freight_mgmt/__manifest__.py
+++ custom_addons/freight_mgmt/__manifest__.py
@@ -21,7 +21,7 @@
 #############################################################################
 {
     'name': "Freight Management",
-    'version': '15.0.2.0.1',
+    'version': '15.0.2.0.2',
     'summary': """Create Freight Management System""",
     'description': """Create a module that allows management all freight operations (Air, Ocean, and Land).""",
     'author': 'Tuan Huynh',
