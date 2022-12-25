9422e3e648fabb95ed14897109505c4184f39ac7
Tuan Huynh <hat726@gmail.com>
2022-12-25 11:35:23 +0700

    * mail_activity_board module: changes to mail_activity_view.xml

--- custom_addons/mail_activity_board/__manifest__.py
+++ custom_addons/mail_activity_board/__manifest__.py
@@ -1,5 +1,6 @@
 # Copyright 2018 David Juaneda - <djuaneda@sdi.es>
 # Copyright 2021 Sodexis
+# Copyright 2022 Tuan Huynh
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Mail Activity Board",
@@ -7,18 +8,18 @@
     "version": "15.0.1.1.0",
     "development_status": "Beta",
     "category": "Social Network",
-    "website": "https://github.com/OCA/social",
-    "author": "SDi, David Juaneda, Sodexis, ACSONE SA/NV, Odoo Community Association (OCA)",
+    "website": "https://odoo-community.org/",
+    "author": "Tuan Huynh, Bao Thinh Ltd., Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
-    "depends": ["calendar", "board"],
+    "depends": ["calendar"],
     "data": ["views/mail_activity_view.xml"],
-    "assets": {
-        "web.assets_backend": [
-            "mail_activity_board/static/src/components/chatter_topbar/chatter_topbar.esm.js",
-        ],
-        "web.assets_qweb": [
-            "mail_activity_board/static/src/components/chatter_topbar/chatter_topbar.xml",
-        ],
-    },
+    # "assets": {
+    #     "web.assets_backend": [
+    #         "mail_activity_board/static/src/components/chatter_topbar/chatter_topbar.esm.js",
+    #     ],
+    #     "web.assets_qweb": [
+    #         "mail_activity_board/static/src/components/chatter_topbar/chatter_topbar.xml",
+    #     ],
+    # },
 }
