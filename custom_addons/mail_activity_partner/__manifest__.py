f0cb24fc573f909e1cf5209777894cd9ab505cd9
Tuan Huynh <hat726@gmail.com>
2022-12-25 11:41:15 +0700

    * 20220721 - mail_activity_partner module - Changes to activities adding more information to popup

--- custom_addons/mail_activity_partner/__manifest__.py
+++ custom_addons/mail_activity_partner/__manifest__.py
@@ -1,4 +1,5 @@
 # Copyright 2018 ForgeFlow S.L.
+# Copyright 2022 Tuan Huynh
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Mail Activity Partner",
@@ -6,8 +7,8 @@
     "version": "15.0.1.0.0",
     "development_status": "Beta",
     "category": "Social Network",
-    "website": "https://github.com/OCA/social",
-    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "website": "https://odoo-community.org/",
+    "author": "Tuan Huynh, Bao Thinh Ltd., Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
     "data": ["views/mail_activity_views.xml"],
