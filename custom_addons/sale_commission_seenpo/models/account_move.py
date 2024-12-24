89e6054d60c320bbda24e04a6605ede3ca45accc
Tuan Huynh <hat726@gmail.com>
2024-12-25 04:34:41 +1300

    * 20241225 - Deploy bug fixed of missing amount_custom in invoice line agent.

--- custom_addons/sale_commission_seenpo/models/account_move.py
+++ custom_addons/sale_commission_seenpo/models/account_move.py
@@ -1,10 +1,16 @@
 # Copyright 2022 Bao Thinh Software - Tuan Huynh
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models
+from odoo import models, fields
 
 
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
 
+class AccountMoveLineAgent(models.Model):
+    _inherit = "account.invoice.line.agent"
+    _description = "Agent detail of commission line in account invoice lines"
+
+    amount_custom = fields.Float(string="Commission Amount Custom", default=0.0,
+                                 store=True, tracking=True)
